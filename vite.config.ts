bb858eb6b7de96831a059a786ec3d8ddc93f306b
James Mosley <jimmosley90@gmail.com>
2025-04-16 14:11:33 +0300

    modification in json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve:{
-    alias:{
-      crypto: "crypto-browserify"
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
+  optimizeDeps: {
+    include: ['crypto-browserify'],
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
     },
   },
 });
