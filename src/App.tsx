8b91d22cb90e210c29a9f6ee2296ae769bb638be
James Mosley <jimmosley90@gmail.com>
2025-04-29 08:33:24 +0300

    adjustments on APP.TsX

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
@@ -6,21 +5,23 @@ import ForgotPassword from "./components/ForgotPassword";
 import Settings from "./components/Settings";
 import Password from "./components/Password";
 import { Box, VStack, Link as ChakraLink } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Box p={4}>
-        <VStack  align="start">
-          <ChakraLink href="/dashboard">Dashboard</ChakraLink>
-          <ChakraLink href="/login">Login</ChakraLink>
-          <ChakraLink href="/forgot-password">Forgot Password</ChakraLink>
-          <ChakraLink href="/settings">Settings</ChakraLink>
-          <ChakraLink href="/password">Change Password</ChakraLink>
-          </VStack>
+        <VStack align="start">
+          <ChakraLink as={Link} to="/dashboard">Dashboard</ChakraLink>
+          <ChakraLink as={Link} to="/login">Login</ChakraLink>
+          <ChakraLink as={Link} to="/forgot-password">Forgot Password</ChakraLink>
+          <ChakraLink as={Link} to="/settings">Settings</ChakraLink>
+          <ChakraLink as={Link} to="/password">Change Password</ChakraLink>
+        </VStack>
       </Box>
 
       <Routes>
+        <Route path="/" element={<Login />} /> {/* NEW */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
